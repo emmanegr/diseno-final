5e0339f087a7a81a31ecc853d146bd66ae5aa11f
jpnava <jp.nava.u@gmail.com>
2012-05-02 18:16:42 -0500

    cosas

--- app/models/concurso.rb
+++ app/models/concurso.rb
@@ -1,5 +1,6 @@
 class Concurso < ActiveRecord::Base
-  belongs_to :categoria
+
+  has_and_belongs_to_many :categorias
   has_attached_file :fotoconcurso, :styles => { :big => "266x266#", :thumb => "100x100#" }
 
 end
