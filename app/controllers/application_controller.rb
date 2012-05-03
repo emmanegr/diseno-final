09ea0fc75fadd387ab36a1e615a82d0376d6df5b
Emmanuel Negrete <emmanegr@gmail.com>
2012-05-02 20:43:19 -0500

    whololo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :current_user_session, :current_user
+  helper_method :current_user_session, :current_user, :require_user
 
   private
     def current_user_session
