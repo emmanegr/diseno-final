0ba087fb4ae6dedb9fd6ff377f12565d8b4f236c
Emmanuel Negrete <emmanegr@gmail.com>
2012-05-03 08:11:56 -0500

    wow!

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :categorias
   belongs_to :user
   has_many :comentarios
-  has_attached_file :fotopost1, :styles => { :big => "600x400#", :thumb => "202x158#" }
+  has_attached_file :fotopost1, :styles => { :ultraBig => "800x600#", :big => "600x400#", :thumb => "202x158#" }
   has_attached_file :fotopost2, :styles => { :big => "600x400#", :thumb => "202x158#" }
   has_attached_file :fotopost3, :styles => { :big => "600x400#", :thumb => "202x158#" }
   has_attached_file :fotopost4, :styles => { :big => "600x400#", :thumb => "202x158#" }
